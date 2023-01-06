1b70042de19c8021bed74276eec3874a44ac3469
Wenyu520521 <2584107537@qq.com>
2023-01-06 22:35:06 +0800

    predict.py是车牌识别算法文件 1.编写了imreadex方法读取图片文件 2.编写了find_waves方法，根据设定的阈值和图片直方图，找出波峰，用于分割字符

---
+++ predict.py
@@ -0,0 +1,44 @@
+import json
+import os
+
+import cv2
+import numpy as np
+from numpy.linalg import norm
+
+SZ = 20  # 训练图片长宽
+MAX_WIDTH = 1000  # 原始图片最大宽度
+Min_Area = 2000  # 车牌区域允许最大面积
+PROVINCE_START = 1000
+
+
+# 读取图片文件
+def imreadex(filename):
+    return cv2.imdecode(np.fromfile(filename, dtype=np.uint8), cv2.IMREAD_COLOR)
+
+
+def point_limit(point):
+    if point[0] < 0:
+        point[0] = 0
+    if point[1] < 0:
+        point[1] = 0
+
+
+# 根据设定的阈值和图片直方图，找出波峰，用于分隔字符
+def find_waves(threshold, histogram):
+    up_point = -1  # 上升点
+    is_peak = False
+    if histogram[0] > threshold:
+        up_point = 0
+        is_peak = True
+    wave_peaks = []
+    for i, x in enumerate(histogram):
+        if is_peak and x < threshold:
+            if i - up_point > 2:
+                is_peak = False
+                wave_peaks.append((up_point, i))
+        elif not is_peak and x >= threshold:
+            is_peak = True
+            up_point = i
+    if is_peak and up_point != -1 and i - up_point > 4:
+        wave_peaks.append((up_point, i))
+    return wave_peaks
